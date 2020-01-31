6f41c22944c8676bfe58998f599f52ee4daff5c3
dbuechel <damian.buechel@let.ethz.ch>
2020-01-31 13:57:47 +0100

    Implemented basic functionality.

---
+++ PortalPageApplication/Handlers/DisplayHandler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Windows.Forms;
+using CefSharp;
+
+namespace PortalPageApplication.Handlers
+{
+	internal class DisplayHandler : IDisplayHandler
+	{
+		private Form form;
+		private HttpClient httpClient;
+
+		internal DisplayHandler(Form form)
+		{
+			this.form = form;
+			this.httpClient = new HttpClient();
+		}
+
+		public void OnAddressChanged(IWebBrowser chromiumWebBrowser, AddressChangedEventArgs addressChangedArgs)
+		{
+		}
+
+		public bool OnAutoResize(IWebBrowser chromiumWebBrowser, IBrowser browser, CefSharp.Structs.Size newSize)
+		{
+			return false;
+		}
+
+		public bool OnConsoleMessage(IWebBrowser chromiumWebBrowser, ConsoleMessageEventArgs consoleMessageArgs)
+		{
+			return false;
+		}
+
+		public void OnFaviconUrlChange(IWebBrowser chromiumWebBrowser, IBrowser browser, IList<string> urls)
+		{
+			var request = new HttpRequestMessage(HttpMethod.Get, urls.First());
+			var response = httpClient.SendAsync(request).ContinueWith(task =>
+			{
+				if (task.IsCompleted && task.Result.IsSuccessStatusCode)
+				{
+					task.Result.Content.ReadAsStreamAsync().ContinueWith(stream =>
+					{
+						if (stream.IsCompleted && Path.GetExtension(request.RequestUri.AbsolutePath).Equals(".ico", StringComparison.OrdinalIgnoreCase))
+						{
+							form.Invoke(new Action(() => form.Icon = new Icon(stream.Result)));
+						}
+					});
+				}
+			});
+		}
+
+		public void OnFullscreenModeChange(IWebBrowser chromiumWebBrowser, IBrowser browser, bool fullscreen)
+		{
+		}
+
+		public void OnLoadingProgressChange(IWebBrowser chromiumWebBrowser, IBrowser browser, double progress)
+		{
+			form.Invoke(new Action(() => form.Cursor = progress < 1.0 ? Cursors.AppStarting : Cursors.Default));
+		}
+
+		public void OnStatusMessage(IWebBrowser chromiumWebBrowser, StatusMessageEventArgs statusMessageArgs)
+		{
+		}
+
+		public void OnTitleChanged(IWebBrowser chromiumWebBrowser, TitleChangedEventArgs titleChangedArgs)
+		{
+		}
+
+		public bool OnTooltipChanged(IWebBrowser chromiumWebBrowser, ref string text)
+		{
+			return true;
+		}
+	}
+}
---
+++ PortalPageApplication/MainWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using PortalPageApplication.Handlers;
+
+namespace PortalPageApplication
+{
+	public partial class MainWindow : Form
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+			var url = ConfigurationManager.AppSettings["StartUrl"];
+			var browser = new ChromiumWebBrowser(url)
+			{
+				Dock = DockStyle.Fill
+			};
+
+			browser.LoadError += Browser_LoadError;
+			browser.DisplayHandler = new DisplayHandler(this);
+			browser.MenuHandler = new ContextMenuHandler();
+			browser.TitleChanged += Browser_TitleChanged;
+
+			Height = Screen.PrimaryScreen.WorkingArea.Height;
+			Location = new Point(Screen.PrimaryScreen.WorkingArea.Width / 2, Screen.PrimaryScreen.WorkingArea.Top);
+			Width = Screen.PrimaryScreen.WorkingArea.Width / 2;
+
+			Controls.Add(browser);
+		}
+
+		private void Browser_TitleChanged(object sender, TitleChangedEventArgs e)
+		{
+			Invoke(new Action(() => Text = e.Title));
+		}
+
+		private void Browser_LoadError(object sender, LoadErrorEventArgs e)
+		{
+			e.Frame.LoadHtml($"<html><body>Failed to load '{e.FailedUrl}'!<br />{e.ErrorText} ({e.ErrorCode})</body></html>");
+		}
+	}
+}
